88f3eb58ddf9ff19496f92dc6c2ac05e42eabb35
spxrtxk <spartakvasilev1999@gmail.com>
2024-10-08 19:46:23 +0900

    feat: add goroutines

---
+++ go.mod
@@ -0,0 +1,11 @@
+module go-parser/mod
+
+go 1.23.0
+
+require (
+	github.com/andybalholm/brotli v1.1.0 // indirect
+	github.com/klauspost/compress v1.17.10 // indirect
+	github.com/pquerna/ffjson v0.0.0-20190930134022-aa0246cd15f7 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.56.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"encoding/csv"
-	"encoding/json"
 	"encoding/xml"
 	"fmt"
-	"io/ioutil"
 	"log"
-	"net/http"
 	"os"
+	"path/filepath"
 	"strings"
+	"sync"
+	"time"
+
+	"github.com/pquerna/ffjson/ffjson"
+	"github.com/valyala/fasthttp"
 )
 
 type Product struct {
@@ -22,6 +25,7 @@ type ProductsResponse struct {
 
 type ProductDetails struct {
 	XMLName  xml.Name `xml:"potravina"`
+	GUID     string   `xml:"guid_potravina,attr"` // Добавляем поле для извлечения guid_potravina
 	Name     string   `xml:"nazev"`
 	Category string   `xml:"kategorie"`
 	Ean      []string `xml:"eans>ean"`
@@ -36,9 +40,8 @@ type ProductDetails struct {
 	GI       string   `xml:"hodnoty>gi"`
 	PHE      string   `xml:"hodnoty>phe"`
 	Alcohol  string   `xml:"hodnoty>alcohol"`
-	Image    string   `xml:"image"`
-	Vitamins []Label  `xml:"stitkyVitaminy>stitek"`
-	Minerals []Label  `xml:"stitkyMineraly>stitek"`
+	Image    string   `xml:"foto"`
+	URL      string   `xml:"url"` // Добавляем поле для проверки блокировки
 }
 
 type Label struct {
@@ -46,28 +49,38 @@ type Label struct {
 	Description string `xml:",chardata"`
 }
 
+type Cache struct {
+	mu    sync.RWMutex
+	cache map[string]ProductDetails
+}
+
+func (c *Cache) Get(key string) (ProductDetails, bool) {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	val, ok := c.cache[key]
+	return val, ok
+}
+
+func (c *Cache) Set(key string, value ProductDetails) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.cache[key] = value
+}
+
 func main() {
-	// Открываем JSON-файл
-	file, err := ioutil.ReadFile("data-0.json")
+	dataDir := "data-4"
+	files, err := os.ReadDir(dataDir)
 	if err != nil {
-		log.Fatalf("Error reading JSON file: %v", err)
+		log.Fatalf("Error reading data directory: %v", err)
 	}
 
-	// Парсим JSON
-	var productsData ProductsResponse
-	if err := json.Unmarshal(file, &productsData); err != nil {
-		log.Fatalf("Error parsing JSON: %v", err)
-	}
-
-	products := productsData.Data
-
-	const recordsPerFile = 500
+	const recordsPerFile = 1000
 	fileIndex := 1
 	recordCount := 0
 
 	// Функция для создания нового CSV файла
 	createCSVFile := func(fileIndex int) (*csv.Writer, *os.File) {
-		fileName := fmt.Sprintf("results/product_details_%d.csv", fileIndex)
+		fileName := fmt.Sprintf("results-4/product_details_%d.csv", fileIndex)
 		csvFile, err := os.Create(fileName)
 		if err != nil {
 			log.Fatalf("Failed to create CSV file: %v", err)
@@ -92,10 +105,6 @@ func main() {
 			"PHE",
 			"Alcohol",
 			"Image",
-			"Vitamins",
-			"VitaminsDescr",
-			"Minerals",
-			"MineralsDescr",
 		}
 		if err := writer.Write(headers); err != nil {
 			log.Fatalf("Failed to write headers to CSV: %v", err)
@@ -107,82 +116,141 @@ func main() {
 	writer, csvFile := createCSVFile(fileIndex)
 	defer csvFile.Close()
 
-	// Обходим все продукты и получаем их детали
-	for _, product := range products {
-		if recordCount >= recordsPerFile {
-			writer.Flush()
-			csvFile.Close()
-			fileIndex++
-			writer, csvFile = createCSVFile(fileIndex)
-			defer csvFile.Close()
-			recordCount = 0
+	// Кэш для результатов
+	cache := &Cache{cache: make(map[string]ProductDetails)}
+
+	// Обрабатываем каждый файл из папки data
+	for _, fileInfo := range files {
+		if fileInfo.IsDir() {
 			continue
 		}
 
-		url := fmt.Sprintf("https://www.dine4fit.com/getPotravina.php?GUID_Potravina=%s&lang=ru&pid=iOShVbzP", product.ID)
-		response, err := http.Get(url)
+		filePath := filepath.Join(dataDir, fileInfo.Name())
+		file, err := os.ReadFile(filePath)
 		if err != nil {
-			log.Printf("Failed to fetch details for ID %s: %v", product.ID, err)
-			continue
-		}
-		defer response.Body.Close()
-
-		var details ProductDetails
-		if err := xml.NewDecoder(response.Body).Decode(&details); err != nil {
-			log.Printf("Failed to decode XML for ID %s: %v", product.ID, err)
+			log.Printf("Error reading JSON file %s: %v", filePath, err)
 			continue
 		}
 
-		// Конкатенируем данные с "|" для полей с множественными значениями
-		eans := strings.Join(details.Ean, "|")
-
-		// Конкатенируем названия и описания витаминов и минералов
-		var vitaminNames, vitaminDescriptions []string
-		for _, vitamin := range details.Vitamins {
-			vitaminNames = append(vitaminNames, vitamin.Name)
-			vitaminDescriptions = append(vitaminDescriptions, vitamin.Description)
-		}
-		vitamins := strings.Join(vitaminNames, "|")
-		vitaminsDescr := strings.Join(vitaminDescriptions, "|")
-
-		var mineralNames, mineralDescriptions []string
-		for _, mineral := range details.Minerals {
-			mineralNames = append(mineralNames, mineral.Name)
-			mineralDescriptions = append(mineralDescriptions, mineral.Description)
-		}
-		minerals := strings.Join(mineralNames, "|")
-		mineralsDescr := strings.Join(mineralDescriptions, "|")
-
-		// Пишем детали в CSV
-		record := []string{
-			product.ID,
-			details.Name,
-			details.Category,
-			eans,
-			details.Energy,
-			details.Protein,
-			details.Fat,
-			details.Carb,
-			details.Salt,
-			details.Water,
-			details.Sugar,
-			details.Calcium,
-			details.GI,
-			details.PHE,
-			details.Alcohol,
-			details.Image,
-			vitamins,
-			vitaminsDescr,
-			minerals,
-			mineralsDescr,
-		}
-		if err := writer.Write(record); err != nil {
-			log.Fatalf("Failed to write record to CSV: %v", err)
-		} else {
-			log.Printf("%v Success! PRODUCT ID: %s", recordCount, product.ID)
+		// Парсим JSON
+		var productsData ProductsResponse
+		if err := ffjson.Unmarshal(file, &productsData); err != nil {
+			log.Printf("Error parsing JSON from file %s: %v", filePath, err)
+			continue
 		}
 
-		recordCount++
+		products := productsData.Data
+
+		// Канал для управления потоком данных
+		productChan := make(chan Product, len(products))
+		resultChan := make(chan ProductDetails, len(products))
+
+		// Запускаем горутины для обработки продуктов
+		var wg sync.WaitGroup
+		for i := 0; i < 10; i++ { // Устанавливаем количество горутин
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				for product := range productChan {
+					if details, ok := cache.Get(product.ID); ok {
+						resultChan <- details
+						continue
+					}
+
+					url := fmt.Sprintf("https://www.dine4fit.com/getPotravina.php?GUID_Potravina=%s&lang=ru&pid=iOShVbzP", product.ID)
+					var details ProductDetails
+					var err error
+					for attempt := 0; attempt < 3; attempt++ { // Максимум 3 попытки
+						req := fasthttp.AcquireRequest()
+						resp := fasthttp.AcquireResponse()
+						defer fasthttp.ReleaseRequest(req)
+						defer fasthttp.ReleaseResponse(resp)
+
+						req.SetRequestURI(url)
+						if err = fasthttp.Do(req, resp); err != nil {
+							log.Printf("Failed to fetch details for ID %s (attempt %d): %v", product.ID, attempt+1, err)
+							time.Sleep(time.Second * time.Duration(attempt+1)) // Задержка перед повторной попыткой
+							continue
+						}
+
+						if err = xml.Unmarshal(resp.Body(), &details); err != nil {
+							log.Printf("Failed to decode XML for ID %s (attempt %d): %v", product.ID, attempt+1, err)
+							time.Sleep(time.Second * time.Duration(attempt+1)) // Задержка перед повторной попыткой
+							continue
+						}
+
+						// Проверяем, заблокирован ли продукт
+						if details.URL == "" {
+							log.Printf("Skipping product ID %s with empty URL", product.ID)
+							break
+						}
+
+						cache.Set(product.ID, details)
+						resultChan <- details
+						break
+					}
+					if err != nil {
+						log.Printf("Failed to fetch details for ID %s after 3 attempts", product.ID)
+					}
+				}
+			}()
+		}
+
+		// Отправляем продукты в канал
+		go func() {
+			for _, product := range products {
+				productChan <- product
+			}
+			close(productChan)
+		}()
+
+		// Ожидаем завершения всех горутин
+		go func() {
+			wg.Wait()
+			close(resultChan)
+		}()
+
+		// Обрабатываем результаты
+		for details := range resultChan {
+			if recordCount >= recordsPerFile {
+				writer.Flush()
+				csvFile.Close()
+				fileIndex++
+				writer, csvFile = createCSVFile(fileIndex)
+				defer csvFile.Close()
+				recordCount = 0
+			}
+
+			// Конкатенируем данные с "|" для полей с множественными значениями
+			eans := strings.Join(details.Ean, "|")
+
+			// Пишем детали в CSV
+			record := []string{
+				details.GUID, // Используем поле GUID для записи значения guid_potravina
+				details.Name,
+				details.Category,
+				eans,
+				details.Energy,
+				details.Protein,
+				details.Fat,
+				details.Carb,
+				details.Salt,
+				details.Water,
+				details.Sugar,
+				details.Calcium,
+				details.GI,
+				details.PHE,
+				details.Alcohol,
+				details.Image,
+			}
+			if err := writer.Write(record); err != nil {
+				log.Fatalf("Failed to write record to CSV: %v", err)
+			} else {
+				log.Printf("%v Success! PRODUCT ID: %s", recordCount, details.GUID)
+			}
+
+			recordCount++
+		}
 	}
 
 	writer.Flush()
