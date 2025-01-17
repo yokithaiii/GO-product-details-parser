a5a4d67acdae32a462cff8d113cc0c098b0130e8
spxrtxk <spartakvasilev1999@gmail.com>
2025-01-17 12:15:25 +0900

    fix: code style

--- main.go
+++ main.go
@@ -25,7 +25,7 @@ type ProductsResponse struct {
 
 type ProductDetails struct {
 	XMLName  xml.Name `xml:"potravina"`
-	GUID     string   `xml:"guid_potravina,attr"` // Добавляем поле для извлечения guid_potravina
+	GUID     string   `xml:"guid_potravina,attr"`
 	Name     string   `xml:"nazev"`
 	Category string   `xml:"kategorie"`
 	Ean      []string `xml:"eans>ean"`
@@ -41,7 +41,7 @@ type ProductDetails struct {
 	PHE      string   `xml:"hodnoty>phe"`
 	Alcohol  string   `xml:"hodnoty>alcohol"`
 	Image    string   `xml:"foto"`
-	URL      string   `xml:"url"` // Добавляем поле для проверки блокировки
+	URL      string   `xml:"url"`
 }
 
 type Label struct {
@@ -68,7 +68,7 @@ func (c *Cache) Set(key string, value ProductDetails) {
 }
 
 func main() {
-	dataDir := "data-4"
+	dataDir := "data"
 	files, err := os.ReadDir(dataDir)
 	if err != nil {
 		log.Fatalf("Error reading data directory: %v", err)
@@ -78,16 +78,14 @@ func main() {
 	fileIndex := 1
 	recordCount := 0
 
-	// Функция для создания нового CSV файла
 	createCSVFile := func(fileIndex int) (*csv.Writer, *os.File) {
-		fileName := fmt.Sprintf("results-4/product_details_%d.csv", fileIndex)
+		fileName := fmt.Sprintf("results/product_details_%d.csv", fileIndex)
 		csvFile, err := os.Create(fileName)
 		if err != nil {
 			log.Fatalf("Failed to create CSV file: %v", err)
 		}
 		writer := csv.NewWriter(csvFile)
 
-		// Пишем заголовки в CSV
 		headers := []string{
 			"ID",
 			"Name",
@@ -116,10 +114,8 @@ func main() {
 	writer, csvFile := createCSVFile(fileIndex)
 	defer csvFile.Close()
 
-	// Кэш для результатов
 	cache := &Cache{cache: make(map[string]ProductDetails)}
 
-	// Обрабатываем каждый файл из папки data
 	for _, fileInfo := range files {
 		if fileInfo.IsDir() {
 			continue
@@ -132,7 +128,6 @@ func main() {
 			continue
 		}
 
-		// Парсим JSON
 		var productsData ProductsResponse
 		if err := ffjson.Unmarshal(file, &productsData); err != nil {
 			log.Printf("Error parsing JSON from file %s: %v", filePath, err)
@@ -141,13 +136,11 @@ func main() {
 
 		products := productsData.Data
 
-		// Канал для управления потоком данных
 		productChan := make(chan Product, len(products))
 		resultChan := make(chan ProductDetails, len(products))
 
-		// Запускаем горутины для обработки продуктов
 		var wg sync.WaitGroup
-		for i := 0; i < 10; i++ { // Устанавливаем количество горутин
+		for i := 0; i < 10; i++ {
 			wg.Add(1)
 			go func() {
 				defer wg.Done()
@@ -160,7 +153,7 @@ func main() {
 					url := fmt.Sprintf("https://www.dine4fit.com/getPotravina.php?GUID_Potravina=%s&lang=ru&pid=iOShVbzP", product.ID)
 					var details ProductDetails
 					var err error
-					for attempt := 0; attempt < 3; attempt++ { // Максимум 3 попытки
+					for attempt := 0; attempt < 3; attempt++ {
 						req := fasthttp.AcquireRequest()
 						resp := fasthttp.AcquireResponse()
 						defer fasthttp.ReleaseRequest(req)
@@ -169,17 +162,16 @@ func main() {
 						req.SetRequestURI(url)
 						if err = fasthttp.Do(req, resp); err != nil {
 							log.Printf("Failed to fetch details for ID %s (attempt %d): %v", product.ID, attempt+1, err)
-							time.Sleep(time.Second * time.Duration(attempt+1)) // Задержка перед повторной попыткой
+							time.Sleep(time.Second * time.Duration(attempt+1))
 							continue
 						}
 
 						if err = xml.Unmarshal(resp.Body(), &details); err != nil {
 							log.Printf("Failed to decode XML for ID %s (attempt %d): %v", product.ID, attempt+1, err)
-							time.Sleep(time.Second * time.Duration(attempt+1)) // Задержка перед повторной попыткой
+							time.Sleep(time.Second * time.Duration(attempt+1))
 							continue
 						}
 
-						// Проверяем, заблокирован ли продукт
 						if details.URL == "" {
 							log.Printf("Skipping product ID %s with empty URL", product.ID)
 							break
@@ -196,7 +188,6 @@ func main() {
 			}()
 		}
 
-		// Отправляем продукты в канал
 		go func() {
 			for _, product := range products {
 				productChan <- product
@@ -204,13 +195,11 @@ func main() {
 			close(productChan)
 		}()
 
-		// Ожидаем завершения всех горутин
 		go func() {
 			wg.Wait()
 			close(resultChan)
 		}()
 
-		// Обрабатываем результаты
 		for details := range resultChan {
 			if recordCount >= recordsPerFile {
 				writer.Flush()
@@ -221,12 +210,10 @@ func main() {
 				recordCount = 0
 			}
 
-			// Конкатенируем данные с "|" для полей с множественными значениями
 			eans := strings.Join(details.Ean, "|")
 
-			// Пишем детали в CSV
 			record := []string{
-				details.GUID, // Используем поле GUID для записи значения guid_potravina
+				details.GUID,
 				details.Name,
 				details.Category,
 				eans,
